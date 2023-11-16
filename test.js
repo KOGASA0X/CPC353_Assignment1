e7f6ea70af5a1f7138349140a345a953356a0826
KOGASA0X <misaka17447@gmail.com>
2023-11-16 17:45:41 +0800

    Add package.json and test.js files, and CPC353 Assignment 1 report

---
+++ test.js
@@ -0,0 +1,27 @@
+import gplay from "google-play-scraper";
+import fss from "fs";
+
+// This example will return 3000 reviews
+// on a single call
+gplay.reviews({
+  appId: 'my.com.tngdigital.ewallet',
+  sort: gplay.sort.HELPFULNESS,
+  num: 3000
+}).then(
+  (reviews) => {
+    // Convert reviews to JSON string for logging
+    const reviewsString = JSON.stringify(reviews, null, 2);
+
+    // Write the reviews to the log file
+    fss.appendFile('log.txt', reviewsString, (err) => {
+      if (err) {
+        console.error('Error writing to log file:', err);
+      } else {
+        console.log('Reviews logged to', 'log.txt');
+      }
+    });
+  },
+  (error) => {
+    console.error('Error fetching reviews:', error);
+  }
+);
